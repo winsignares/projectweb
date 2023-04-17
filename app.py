5163ef00b64c96c19054793c0485c7b69efb7943
= <=>
2023-04-16 20:22:09 -0500

    consultas 2 y 3 tablas con filtro

--- app.py
+++ app.py
@@ -53,5 +53,46 @@ def actualizaruser():
     bd.session.commit()     
     return "actualización exitosa"
 
+
+@app.route('/dostablas', methods=['GET'])
+def dostablas():
+    results = bd.session.query(Users, Taks).join(Taks).all() 
+    dato={}   
+    i=0
+    for users, taks in results:
+        i+=1	       
+        dato[i] = {
+        'Nombre':users.fullname,
+		'email':users.email,
+		'Nombre Tarea':taks.nametak,                     
+        }
+        print(users.fullname, taks.nametak)
+   
+    #my_bytes = 'hello world'.encode('utf-8')
+
+# ⛔️ TypeError: Object of type bytes is not JSON serializable
+   
+    return jsonify(dato)
+    
+
+@app.route('/trestablas',methods=['GET'])
+def trestabla():
+    results = bd.session.query(Users, Taks, Category). \
+    select_from(Users).join(Taks).join(Category).all()
+    for users, taks, category in results:
+        print(users.fullname, taks.nametak, category.namecategory)
+    return "Dato"
+
+@app.route('/trestablaconfiltro', methods=['GET'])
+def trestablaconfiltro():
+    results = bd.session.query(Users.fullname, Users.email).join(Taks).join(Category). \
+    filter(Users.id == 1).all()
+
+    for result in results:
+        print(result)
+    return 'Dato'
+
+# resultado = db.session.query(TblUsuarios, tblrolesusuarios).select_from(TblUsuarios.Cedula, TblUsuarios.full_name, TblUsuarios.telefono, TblUsuarios.cargo, tblrolesusuarios.rol).join(tblrolesusuarios).filter(tblrolesusuarios.roles== "personal").all()
+   
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
