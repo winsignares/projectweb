f1992ce5bd3feac0750e4d3e8e12f892aa6ec8f3
Martin Cantillo <martinjosecantillohernandez9@gmail.com>
2023-05-08 08:52:55 -0500

    m

---
+++ config/static/js/user.js
@@ -0,0 +1,10 @@
+//var es para trabajar variables de tipo global
+//let  se declara dentro de los metodos osea es de manera local
+//const se utiliza en cuestion de objetos
+
+
+function saveUser(){
+
+}
+
+//cuando se cree siempre un js el sccrip siempre se tiene que colocar en el html
